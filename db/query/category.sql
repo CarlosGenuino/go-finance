ef2accf2fc6d4f90c0dc6a50123caecc2b91ad88
Carlos Genuino <carloxsgenuino3@live.com>
2023-01-18 12:18:26 -0300

    resolvendo sqlc

--- db/query/category.sql
+++ db/query/category.sql
@@ -1,14 +1,14 @@
 -- name: CreateCategory :one
-INSERT INTO "categories" (user_id, title, type, description) values ($1, $2, $3, $4) RETURNING *;
+INSERT INTO CATEGORIES (user_id, title, type, description) values ($1, $2, $3, $4) RETURNING *;
 
 -- name: GetCategoryById :one
-SELECT * FROM "categories" WHERE id = $1 LIMIT 1; 
+SELECT * FROM CATEGORIES WHERE id = $1 LIMIT 1; 
 
 -- name: GetCategoryByUserId :many
-SELECT * FROM "categories" WHERE user_id = $1 and type = $2 and title like $3 and description like $4;
+SELECT * FROM CATEGORIES WHERE user_id = $1 and type = $2 and title like $3 and description like $4;
 
 -- name: UpdateCategory :exec
-UPDATE "categories" SET title = $2, description = $3 WHERE id = $1;
+UPDATE CATEGORIES SET title = $2, description = $3 WHERE id = $1;
 
 -- name: DeleteCategory :exec
-DELETE FROM "categories" WHERE id = $1;
+DELETE FROM CATEGORIES WHERE id = $1;
